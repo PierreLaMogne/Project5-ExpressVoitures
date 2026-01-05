f300b85f548b0b5f466c027a9dcefa4c2dce699f
Pierre LaMogne <pierrelamogne@gmail.com>
2026-01-05 23:00:25 +0100

    Ajoutez des fichiers projet.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Net_P5.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
