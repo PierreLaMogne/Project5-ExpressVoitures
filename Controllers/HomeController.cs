1e856a28e33256de376215c0ea0cdbf5184289a0
Pierre LaMogne <pierrelamogne@gmail.com>
2026-01-06 11:49:15 +0100

    Ajout de la vue Create (avec CreateConfirmation)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,19 +1,20 @@
 using System.Diagnostics;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Net_P5.Data;
 using Net_P5.Models;
-using SQLitePCL;
 
 namespace Net_P5.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public HomeController(ApplicationDbContext context)
+        private readonly IWebHostEnvironment _env;
+        public HomeController(ApplicationDbContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
 
         [HttpGet]
@@ -40,9 +41,60 @@ namespace Net_P5.Controllers
             return View(voiture);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            await PopulateDropdowns();
+            return View();
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Voiture voiture)
+        {
+            if (!ModelState.IsValid)
+            {
+                await PopulateDropdowns();
+                return View(voiture);
+            }
 
+            if (await _context.Voitures.AnyAsync(v => v.MarqueId != voiture.Modele.MarqueId))
+            {
+                ModelState.AddModelError(string.Empty, "Le modèle choisi n'appartient pas à la marque sélectionnée");
+                await PopulateDropdowns();
+                return View(voiture);
+            }
 
+            if (await _context.Voitures.AnyAsync(v => v.ModeleId != voiture.Finition.ModeleId))
+            {
+                ModelState.AddModelError(string.Empty, "La finition choisie n'appartient pas au modèle sélectionné");
+                await PopulateDropdowns();
+                return View(voiture);
+            }
+
+            if (voiture.Photo != null && voiture.Photo.Length > 0)
+            {
+                var uploadsFolder = Path.Combine(_env.WebRootPath, "uploads");
+                Directory.CreateDirectory(uploadsFolder);
+                var uniqueFileName = Guid.NewGuid().ToString() + "_" + voiture.Photo.FileName;
+                var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await voiture.Photo.CopyToAsync(fileStream);
+                }
+                voiture.PhotoUrl = "/uploads/" + uniqueFileName;
+            }
+
+            _context.Add(voiture);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(CreateConfirmation));
+        }
+
+        [HttpGet]
+        public IActionResult CreateConfirmation()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
@@ -53,6 +105,13 @@ namespace Net_P5.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task PopulateDropdowns()
+        {
+            ViewBag.Marques = new SelectList(await _context.Marques.ToListAsync(), "Id", "Nom");
+            ViewBag.Modeles = new SelectList(await _context.Modeles.ToListAsync(), "Id", "Nom");
+            ViewBag.Finitions = new SelectList(await _context.Finitions.ToListAsync(), "Id", "Nom");
         }
     }
 }
