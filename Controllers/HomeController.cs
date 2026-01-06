e4a4ae14ecec10fd5805befe2cf4702694405500
Pierre LaMogne <pierrelamogne@gmail.com>
2026-01-06 02:23:30 +0100

    Vue Index complète avec feuille de style + login + navbar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,28 @@
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Net_P5.Data;
 using Net_P5.Models;
+using SQLitePCL;
 
 namespace Net_P5.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var voitures = await _context.Voitures
+                .Include(v => v.Finition.Modele.Marque)
+                .Include(v => v.Reparations)
+                .ToListAsync();
+            return View(voitures);
         }
 
         public IActionResult Privacy()
